5f4297ba94148d1e2ae1f3390736d2ea0df4f4cf
whoami <az.khabibullin2018@gmail.com>
2024-06-04 23:12:47 +0500

    локально создал процедуру procProductListReport и таблицу barcode скопировал класс ProductList со свойствами для таблицы и метод GetProductListReport далее настрою макет отчета DevExpress и проведу отладку кода

---
+++ learn_devexpress/ProductList.cs
@@ -0,0 +1,15 @@
+﻿namespace learn_devexpress
+{
+    public class ProductList
+    {
+        public string typeName { get; set; }
+
+        public int productId { get; set; }
+
+        public string productName { get; set; }
+
+        public decimal Price { get; set; }
+
+        public string barCode { get; set; }
+    }
+}
---
+++ learn_devexpress/clsQuery.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+
+namespace learn_devexpress
+{
+    public class clsQuery
+    {
+        private readonly string stCon = @"Data Source=DESKTOP-QOLE0OQ;Initial Catalog=learn_devexpress;Integrated Security=True";
+
+        public List<ProductList> GetProductListReport()
+        {
+            DataTable result = new DataTable("GetProductListReport");
+
+            List<ProductList> listprod = new List<ProductList>();
+
+            try
+            {
+                using (SqlConnection con = new SqlConnection(stCon))
+                {
+                    using (SqlCommand com = new SqlCommand("procProductListReport", con))
+                    {
+                        com.CommandType = CommandType.StoredProcedure;
+
+                        con.Open();
+
+                        using (var reader = com.ExecuteReader(CommandBehavior.CloseConnection))
+                        {
+                            result.Load(reader);
+                        }
+                    }
+                }
+
+                listprod = result.AsEnumerable().Select(m => new ProductList()
+                {
+                    typeName = m.Field<string>("typeName"),
+                    productId = m.Field<int>("idProduct"),
+                    productName = m.Field<string>("productName"),
+                    Price = m.Field<decimal>("Price"),
+                    barCode = m.Field<string>("barcode"),
+                }).ToList();
+
+
+
+            }
+            catch(Exception ex)
+            {
+                throw;
+            }
+
+            return listprod;
+        }
+    }
+}
